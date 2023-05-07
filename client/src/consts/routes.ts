3f582c59218f128b3912cea177fe477001f12ef1
Gabriel Santos Meireles <dev.meireles@gmail.com>
2023-05-07 19:47:00 +0100

    reseting vue + vite structure

---
+++ client/src/consts/routes.ts
@@ -0,0 +1,11 @@
+const MAIN_ROUTE = `/dashboard`;
+
+export const routes = {
+  dashboard: {
+    main: `${MAIN_ROUTE}`
+  },
+  report: {
+    main: `${MAIN_ROUTE}/report`,
+    create: `${MAIN_ROUTE}/report/create`
+  }
+};
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,26 @@
 import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import { createPinia } from "pinia";
 
-createApp(App).mount("#app");
+import App from "@/App.vue";
+import router from "@/router";
+import vuetify from "@/plugins/vuetify/index";
+import { loadFonts } from "@/plugins/webfontloader";
+import { installI18n } from "@/i18n";
+import { useCoreStore } from "@/stores/core";
+
+loadFonts();
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+const app = createApp(App).use(router).use(vuetify).use(pinia);
+const i18n = installI18n(app);
+
+app.mount("#app");
+
+const coreStore = useCoreStore();
+
+coreStore.$subscribe((_, state) => {
+  i18n.global.locale = state.language;
+});
